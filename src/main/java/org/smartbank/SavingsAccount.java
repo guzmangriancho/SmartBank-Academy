ce3f91ad1edde9421afd34aa7accdb27716cb3ca
Guzmán González de Riancho <guzmangriancho@gmail.com>
2025-12-18 13:00:14 +0100

    feat: savings account implementation

--- src/main/java/org/smartbank/SavingsAccount.java
+++ src/main/java/org/smartbank/SavingsAccount.java
@@ -1,4 +1,24 @@
 package org.smartbank;
 
-public class SavingsAccount extends BankAccount{
+public class SavingsAccount extends BankAccount {
+    private final double interestRate;
+
+    public SavingsAccount(String accountNumber, String accountHolder, double balance, double interestRate) {
+        super(accountNumber, accountHolder, balance);
+        this.interestRate = interestRate;
+    }
+
+    public void applyMonthlyInterest() {
+        balance += balance * (interestRate / 100);
+    }
+
+    @Override
+    public String getAccountInfo() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(super.getAccountInfo()).append("\n");
+        sb.append("Interest rate: ").append(interestRate).append("%\n")
+                .append("==============================");
+
+        return sb.toString();
+    }
 }
